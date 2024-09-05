2aa3e508581831ef81ea361569582fe492140b12
dannycahyo <dannydwicahyono@gmail.com>
2024-09-06 00:48:16 +0700

    feat: implement ProductTable component

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+export default function ProductTable({ products, onDelete }) {
+  return (
+    <table className="min-w-full bg-white">
+      <thead>
+        <tr>
+          <th className="py-2 px-4 border-b">Name</th>
+          <th className="py-2 px-4 border-b">Price</th>
+          <th className="py-2 px-4 border-b">Quantity</th>
+          <th className="py-2 px-4 border-b">Category</th>
+          <th className="py-2 px-4 border-b">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td className="py-2 px-4 border-b">{product.name}</td>
+            <td className="py-2 px-4 border-b">{product.price}</td>
+            <td className="py-2 px-4 border-b">{product.quantity}</td>
+            <td className="py-2 px-4 border-b">{product.category}</td>
+            <td className="py-2 px-4 border-b">
+              <Link
+                to={`/products/${product.id}`}
+                className="px-2 py-1 text-white bg-green-500 rounded"
+              >
+                Edit
+              </Link>
+              <button
+                onClick={() => onDelete(product.id)}
+                className="px-2 py-1 text-white bg-red-500 rounded ml-2"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
