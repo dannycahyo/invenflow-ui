57103e5394d0960bc54e393fe0a089e36a92cc99
dannycahyo <dannydwicahyono@gmail.com>
2024-09-06 00:15:11 +0700

    feat: update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
+import { Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="space-y-4">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to Invenflow UI
+        </h1>
+        <div className="flex justify-center space-x-4">
+          <Link
+            to="/login"
+            className="px-4 py-2 text-white bg-blue-500 rounded"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="px-4 py-2 text-white bg-blue-500 rounded"
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default App;
